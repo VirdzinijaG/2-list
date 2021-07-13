faa8637d4d8ff4ec1991daaea71655d93f44e9e6
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-07-13 18:20:48 +0300

    Validations

--- js/components/Dictionary.js
+++ js/components/Dictionary.js
@@ -5,6 +5,63 @@ class Dictiniory {
         this.selector = selector;
         this.name = pavadinims;
         this.dictinioryList = list;
+
+        // tai elemntas, kuriame gerneruosime visa turini
+        this.DOM = null;
+    }
+    init() {
+        // patikriname, jog validus this.selector
+        // patikriname jog validus this.dictinioryList
+        // randam this.DOM
+        if(!this.isValidSelector() ||
+        !this.isValidDictionary() ||
+        !this.findTargetElement()) {
+            return false;
+        }
+        
+        this.render();
+        // generuojame turini
+    }
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+        this.selector === '') {
+            console.error('ERROR: selector turi buti ne tuscias tekstas(string)');
+            return false;
+        }
+        return true;
+    }
+    isValidDictionary() {
+        if (!Array.isArray(this.dictinioryList) ||
+        this.dictinioryList.length === 0) {
+            console.error('ERROR: zodynas turi buti ne tuscias array');
+            // console.warn(); ispejimas
+            return false;
+        }
+        return true;
+    }
+    // atliekam paieska
+    findTargetElement() {
+        // console.log('ieskome elemento');
+        // console.log('ieskome elemento pagal: ', this.selector); // this.selector yra .content
+
+        // const elementDOM = document.querySelector(this.selector);
+        // console.log(elementDOM);
+        // if (elementDOM) {
+        //     console.log(elementDOM);
+        // } else {
+        //     console.log('elemento rasti nepavyko');
+        // }
+        this.DOM = document.querySelector(this.selector);
+        if (!this.DOM) {
+            console.error('Pagal pateikta selector, norimo elemento nepavyko rasti');
+            return false;
+        }
+        return true;
+    }
+
+    // turinio generavimas
+    render() {
+        console.log('piesiam turini');
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -10,5 +10,8 @@ import { Dictiniory } from "./components/Dictionary.js";
 
 // kuriamas objektas pagal klase Dictionary,kuris yra kitame faile
 const zodynas = new Dictiniory('.content', 'Anglų-Lietuvių kalbos žodynas', wordsData);
+// const zodynas = new Dictiniory('.content', 'Anglų-Lietuvių kalbos žodynas', 25555); klaidu tikrinimas error
 
-console.log(zodynas); // ispausdinama, kas yra pats zodynas
+// console.log(zodynas); // ispausdinama, kas yra pats zodynas
+
+zodynas.init()
